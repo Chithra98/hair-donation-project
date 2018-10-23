ac0f1bfff222bac103d6c1f1f47f482a2d0d1ebe
Chithra98 <sprlic16@gmail.com>
2018-10-24 01:41:40 +0530

    some changes

--- db/routes/admin/attendance.js
+++ db/routes/admin/attendance.js
@@ -27,6 +27,25 @@ methods.attendancemethods.createtable(att)
 })
 
 })
+
+router.delete('/delete', (req, res) => {
+    const info = {};
+    info.lhadmno = req.body.lhadmno;
+    methods.attendancemethods.deleteAtt(info)
+      .then((model) => {
+        res.status(200).json({
+          status: 'Attendance Deleted',
+          state: model,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          status: 'Not able to delete.The row may not exist.',
+          state: err,
+        });
+      });
+  });
+  
 //router.use(('/authenticate'),require('./authentication'));
 //router.use(('/admin'),require('./admin'));
 
